99f52eb49c5b89491a655036f36aae481f026861
emsad87 <emsad87@gmail.com>
2021-06-04 18:30:00 +0200

    Command aliases

--- index.js
+++ index.js
@@ -28,9 +28,10 @@ client.on('message', (message) => {
 	const args = message.content.slice(prefix.length).trim().split(/ +/);
 	const commandName = args.shift().toLowerCase();
 
-	if (!client.commands.has(commandName)) return;
+	const command = client.commands.get(commandName)
+	|| client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-	const command = client.commands.get(commandName);
+	if (!command) return;
 
 	if (command.guildOnly && message.channel.type === 'dm') {
 		return message.reply('I can\'t execute that command inside DMs!');
